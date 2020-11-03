8ab9f703d49ea75535d17513df1fb58f9be3128e
vmr <antonio@poiesis.ws>
2020-11-03 10:26:03 +0100

    Now it is possible to load hex code files with extension .evm_h

--- ghidra_evm/src/main/java/ghidra_evm/ghidra_evmLoader.java
+++ ghidra_evm/src/main/java/ghidra_evm/ghidra_evmLoader.java
@@ -17,6 +17,8 @@ package ghidra_evm;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ghidra.app.util.Option;
 import ghidra.app.util.bin.ByteProvider;
@@ -37,16 +39,22 @@ import ghidra.program.model.mem.MemoryBlock;
  */
 public class ghidra_evmLoader extends AbstractLibrarySupportLoader {
 
+	boolean isEVM = false;
+	boolean isEVM_h = false; 
+	
 	@Override
 	public String getName() {
-		return "Simple evm extension loader";
+		return "EVM bytecode loader";
 	}
 	
 	@Override
 	public Collection<LoadSpec> findSupportedLoadSpecs(ByteProvider provider) throws IOException {
 		List<LoadSpec> loadSpecs = new ArrayList<>();
 		
-		if (provider.getName().endsWith(".evm")) {
+		this.isEVM = provider.getName().endsWith(".evm");
+		this.isEVM_h = provider.getName().endsWith(".evm_h");
+		
+		if (this.isEVM || this.isEVM_h) {
             loadSpecs.add(new LoadSpec(this, 0,
                     new LanguageCompilerSpecPair("evm:BE:64:default", "default"), true));
 		}
@@ -54,6 +62,8 @@ public class ghidra_evmLoader extends AbstractLibrarySupportLoader {
 		return loadSpecs;
 	}
 
+	/* Mainly based on The Ghidra Book, Eagle et al. pp. 389 */
+	
 	@Override
 	protected void load(ByteProvider provider, LoadSpec loadSpec, List<Option> options,
 			Program program, TaskMonitor monitor, MessageLog log)
@@ -61,16 +71,48 @@ public class ghidra_evmLoader extends AbstractLibrarySupportLoader {
 		
 		FlatProgramAPI flatAPI = new FlatProgramAPI(program);
 		try {
-			monitor.setMessage("EVM code: Starting loading");
-			Address start_addr = flatAPI.toAddr(0x0);
-			MemoryBlock block = flatAPI.createMemoryBlock("ram", start_addr, provider.readBytes(0, provider.length()), false);
+			if (this.isEVM) {
+				monitor.setMessage("EVM code: Starting loading");
+				Address start_addr = flatAPI.toAddr(0x0);
+				MemoryBlock block = flatAPI.createMemoryBlock("ram", start_addr, provider.readBytes(0, provider.length()), false);
 
-			block.setRead(true);
-			block.setWrite(true);
-			block.setExecute(true);
+				block.setRead(true);
+				block.setWrite(true);
+				block.setExecute(true);
 		
-			flatAPI.addEntryPoint(start_addr);
-			monitor.setMessage("EVM code: Completed loading");
+				flatAPI.addEntryPoint(start_addr);
+				monitor.setMessage("EVM code: Completed loading");
+			} else if (this.isEVM_h) {
+				monitor.setMessage("EVM code: Starting loading");				
+				
+				CharSequence provider_char_seq =
+						new String(provider.readBytes(0, provider.length()), "UTF-8");
+				Pattern p = Pattern.compile("[0-9a-fA-F]{2}");
+				Matcher m = p.matcher(provider_char_seq);				
+				int match_count = 0;
+				while (m.find()) { match_count++;}
+				m.reset();
+				
+				byte[] evm_byte_code_hex = new byte[match_count];
+				int i = 0;
+				while (m.find()) {
+					String hex_digits = m.group();
+					evm_byte_code_hex[i++] = (byte)Integer.parseInt(hex_digits, 16);
+				}			
+				
+				Address start_addr = flatAPI.toAddr(0x0);
+				MemoryBlock block =
+						flatAPI.createMemoryBlock("ram", start_addr, evm_byte_code_hex, false);
+
+				block.setRead(true);
+				block.setWrite(true);
+				block.setExecute(true);
+		
+				flatAPI.addEntryPoint(start_addr);
+				monitor.setMessage("EVM code: Completed loading");
+			} else {
+				throw new IOException("Failed to load EVM code");
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new IOException("Failed to load EVM code");
